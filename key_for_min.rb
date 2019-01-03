07ff5483c84e137f4f0f9cb5711d3b343e92ce45
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-03 00:37:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -22,15 +22,4 @@ def key_for_min_value(name_hash)
 
   key_for_min_value(name_hash)
 
-  # def key_for_min_value(hash)
-  #   lowest_key = nil
-  #   lowest_value = nil
-  #   hash.each do |k, v|
-  #     if lowest_value == nil || v < lowest_value
-  #       lowest_value = v
-  #       lowest_key = k
-  #     end
-  #   end
-  #   lowest_key
-  # end
 end
